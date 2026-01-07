0ca3514b7386520ed4028ef5aba400ff17aafce6
LeonardoVargesF <leonardovargesf@gmail.com>
2026-01-07 12:19:25 -0300

    Angular Skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:4200" , "http://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
