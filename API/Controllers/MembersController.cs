721fb65e2b7bdbca8f70621fe230a85153160f9e
LeonardoVargesF <leonardovargesf@gmail.com>
2026-01-20 18:09:57 -0300

    20.1

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,14 +1,14 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MembersController(AppDbContext context) : ControllerBase
+    
+    public class MembersController(AppDbContext context) : BaseApiController
     {
         [HttpGet] // localhost:5001/api/members
         public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
@@ -18,6 +18,7 @@ namespace API.Controllers
             return members;
         }
 
+        [Authorize]
         [HttpGet("{Id}")] // localhost:5001/api/members/id
         public async Task<ActionResult<AppUser>>GetMember(string id)
         {
